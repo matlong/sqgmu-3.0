d1a3d1e1c5a1818655522a356a7b9a11d7d2f28e
Long Li <tracylongli@gmail.com>
2021-03-26 17:00:46 +0100

    Add files via upload

---
+++ init.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%
+% Initialisation
+%%%%%%%%%%%%%%%%%%
+%
+% Modified by P. DERIAN 2016-10-10
+%   - added "isdeployed" for compilation support
+% Modified by P. DERIAN 2017-03-10
+%   - removed "clear all"
+
+% Cleaning
+close all; clc;
+
+% Note: isdeployed necessary for compiled code (addpath won't work)
+if ~isdeployed
+    % Debug
+    dbstop if error;
+
+    % Paths
+    fct = genpath([ pwd '/functions' ]);
+    addpath(pwd)
+    addpath(fct)
+    clear fct
+end
+    
+home;
